293235a83f489a0c48cd7397cc29d3bf381a874a
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2017-08-04 19:26:32 -0300

    add bbc GET example
    Signed-off-by: Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
 import requests
 
-url = 'http://127.0.0.1:5000/university'
-#url = 'http://[::1]:5000/university'
+# bbc server
+url = 'http://www.bbc.com'
+
+# local server
+# url = 'http://127.0.0.1:5000/university'
+
+# local server using IPv6
+# url = 'http://[::1]:5000/university'
 
 req = requests.get(url)
 print req.status_code
 print req.text
 
+'''
 req = requests.post(url, data={'name': 'Universidad de Chile'})
 print req.status_code
 print req.text
@@ -22,3 +29,4 @@ print req.text
 req = requests.delete(url + '/25')
 print req.status_code
 print req.text
+'''
